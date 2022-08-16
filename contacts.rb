57ac5b51a01d43bd7c713791608ca1f4cfbad452
Mwenje <mwenje24@gmail.com>
2022-08-16 19:25:59 +0300

    commit tree cfe75881ad032071d23e520725fa23baba7aea35

--- contacts.rb
+++ contacts.rb
@@ -18,7 +18,13 @@ def contacts
 end
 
 def remove_strawberry(contacts)
-  # your code here!
+  contacts.each do |person, contact_details_hash|
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, data|
+        # binding.pry
+      end
+    end
+  end
 end
 
 # print the output to the terminal for inspection
